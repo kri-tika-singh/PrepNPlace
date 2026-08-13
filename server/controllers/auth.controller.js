410151ddeefbcd1d221824251923b85818568f2e
unknown <singh.44kritika@gmail.com>
2026-08-13 10:40:28 +0530

    fix auth controller re.cookie

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -13,12 +13,11 @@ export const googleAuth = async (req,res) => {
             })
         }
         let token = await genToken(user._id)
-        res.cookie("token" , token , {
-            http:true,
-            secure:false,
-            sameSite:"strict",
-            maxAge:7 * 24 * 60 * 60 * 1000
-        })
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none"
+    })
 
         return res.status(200).json(user)
 
